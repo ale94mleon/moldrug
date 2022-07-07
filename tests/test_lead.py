0cd4aed60f6bc1f6bfcad50a980f0e81c69eb1e2
ale94mleon <ale94mleon@gmail.com>
2022-07-07 09:57:24 +0200

    better pytest

--- tests/test_lead.py
+++ tests/test_lead.py
@@ -1,4 +1,6 @@
-import lead.data as data
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from rdkit import Chem
 from lead import utils, fitness
 from lead.data import receptors, ligands, boxes
 import tempfile, os, gzip, shutil, requests
@@ -6,21 +8,21 @@ from multiprocessing import cpu_count
 
 # Creating a temporal directory
 tmp_path = tempfile.TemporaryDirectory()
-# Getting receptor a ligand information
-ligand = ligands.r_x0161
-receptor = receptors.r_x0161
-receptor_file = os.path.join(tmp_path.name, 'receptor.pdbqt')
-with open(receptor_file, 'w') as r:
-    r.write(receptor)
-box = boxes.r_x0161["A"]
+# Creating receptors files
+r_x0161_file = os.path.join(tmp_path.name, 'r_x0161.pdbqt')
+r_6lu7_file = os.path.join(tmp_path.name, 'r_6lu7.pdbqt')
+with open(r_x0161_file, 'w') as r:
+    r.write(receptors.r_x0161)
+with open(r_6lu7_file, 'w') as r:
+    r.write(receptors.r_6lu7)
+
+
 # Getting the crem data base
 url = "http://www.qsar4u.com/files/cremdb/replacements02_sc2.db.gz"
 r = requests.get(url, allow_redirects=True)
-open(os.path.join(tmp_path,'crem.db.gz'), 'wb').write(r.content)
-
-crem_dbgz_path = os.path.join(tmp_path,'crem.db.gz')
-crem_db_path = '/home/ale/GITLAB/lead/row_data/replacements02_sc2.db'
-
+crem_dbgz_path = os.path.join(tmp_path.name,'crem.db.gz')
+crem_db_path = os.path.join(tmp_path.name,'crem.db')
+open(crem_dbgz_path, 'wb').write(r.content)
 with gzip.open(crem_dbgz_path, 'rb') as f_in:
     with open(crem_db_path, 'wb') as f_out:
         shutil.copyfileobj(f_in, f_out)
@@ -29,10 +31,9 @@ with gzip.open(crem_dbgz_path, 'rb') as f_in:
 
 
 def test_single_receptor():
-
     maxiter = 1
     popsize = 2
-    njobs = 3
+    njobs = 2
     NumbCalls = 1
 
     """For a local optimization we could use
@@ -50,10 +51,10 @@ def test_single_receptor():
     """
     
     out = utils.GA(
-        seed_smiles=ligand,
+        seed_smiles=ligands.r_x0161,
         maxiter=maxiter,
         popsize=popsize,
-        crem_db_path = '/home/ale/GITLAB/bi_crem_database/replacements02_sc2.db',
+        crem_db_path = crem_db_path,#'/home/ale/GITLAB/bi_crem_database/replacements02_sc2.db',
         pc = 1,
         get_similar = False,
         mutate_crem_kwargs = {
@@ -67,24 +68,88 @@ def test_single_receptor():
         costfunc = fitness.Cost,#__CostSimilarity,# __VinaCostLipinski, Cost, __VinaCost, __QedSasVinaCost, CostMultiReceptors
         costfunc_kwargs = {
             'vina_executable': 'vina',
-            'receptor_path': receptor_file,
-            'boxcenter' : box['boxcenter'] ,
-            'boxsize': box['boxsize'],
-            'exhaustiveness': 8,
+            'receptor_path': r_x0161_file,
+            'boxcenter' : boxes.r_x0161["A"]['boxcenter'] ,
+            'boxsize': boxes.r_x0161["A"]['boxsize'],
+            'exhaustiveness': 4,
             'ncores': int(cpu_count() / njobs),
             'num_modes': 1,
-            #'ref_smiles': init_smiles,
         },
         save_pop_every_gen = 20,
-        pop_file_name = os.path.join(tmp_path.name, 'pop')
+        pop_file_name = os.path.join(tmp_path.name, 'pop_test_single_receptor')
         )
 
-    for i in range(NumbCalls):
+    for _ in range(NumbCalls):
         out(njobs = njobs)
 
     for o in out.pop:
         print(o.smiles, o.cost)
-    out.pickle(os.path.join(tmp_path.name, f"result_NumGen_{out.NumGen}_PopSize_{popsize}"), compress=True)
+    out.pickle(os.path.join(tmp_path.name, f"result_test_single_receptor_NumGen_{out.NumGen}_PopSize_{popsize}"), compress=True)
     print(out.to_dataframe())
 
-os.remove(crem_db_path)
+def test_multi_receptor():
+    maxiter = 1
+    popsize = 2
+    njobs = 3
+    NumbCalls = 1
+
+    out = utils.GA(
+        seed_smiles=ligands.r_x0161,
+        maxiter=maxiter,
+        popsize=popsize,
+        crem_db_path = crem_db_path,#'/home/ale/GITLAB/bi_crem_database/replacements02_sc2.db',
+        pc = 1,
+        get_similar = False,
+        mutate_crem_kwargs = {
+            'radius':3,
+            'min_size':1,
+            'max_size':8,
+            'min_inc':-5,
+            'max_inc':3,
+            'ncores':cpu_count(),
+        },
+        costfunc = fitness.CostMultiReceptors,#__CostSimilarity,# __VinaCostLipinski, Cost, __VinaCost, __QedSasVinaCost, CostMultiReceptors
+        costfunc_kwargs = {
+            'receptor_path': [r_x0161_file, r_6lu7_file],
+            'boxcenter' : [boxes.r_x0161["A"]['boxcenter'], boxes.r_6lu7["A"]['boxcenter']],
+            'boxsize': [boxes.r_x0161["A"]['boxsize'], boxes.r_6lu7["A"]['boxsize']],
+            'vina_score_types': ['min', 'max'],
+            'exhaustiveness': 4,
+            'ncores': int(cpu_count() / njobs),
+            'num_modes': 1,
+        },
+        save_pop_every_gen = 20,
+        pop_file_name = os.path.join(tmp_path.name, 'pop_test_multi_receptor')
+        )
+
+    for _ in range(NumbCalls):
+        out(njobs = njobs)
+
+    for o in out.pop:
+        print(o.smiles, o.cost)
+    out.pickle(os.path.join(tmp_path.name, f"result_test_multi_receptor_NumGen_{out.NumGen}_PopSize_{popsize}"), compress=True)
+    print(out.to_dataframe())
+
+def test_local():
+    njobs = 2
+    local = utils.Local(
+        mol = Chem.AddHs(Chem.MolFromSmiles(ligands.r_x0161)),
+        crem_db_path = crem_db_path,
+        grow_crem_kwargs = {
+                'radius':3,
+                'min_atoms':1,
+                'max_atoms':3,
+                'ncores':cpu_count(),
+            },
+        costfunc = fitness.Cost,
+        costfunc_kwargs = {
+            'receptor_path': r_x0161_file,
+            'boxcenter' : boxes.r_x0161["A"]['boxcenter'],
+            'boxsize': boxes.r_x0161["A"]['boxsize'],
+            'exhaustiveness': 8,
+            'ncores': int(cpu_count() / njobs),
+            'num_modes': 1,
+        },
+    )
+    local(njobs = njobs, pick=2)
+    print(local.to_dataframe())
