e543a8aec1e3873210c503590c7c2b3ec00e979a
ale94mleon <ale94mleon@gmail.com>
2022-07-16 11:15:27 +0200

    adding follow jobs capability

--- moldrug/__init__.py
+++ moldrug/__init__.py
@@ -9,7 +9,6 @@ import yaml, argparse, os, inspect, importlib
 
 def run():
     from moldrug import utils
-
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument(
@@ -31,34 +30,82 @@ def run():
     
     with open(args.yaml_file, 'r') as c:
         Config = yaml.safe_load(c)
+    
+    # Checking how many jobs are defined: Main and follows jobs
+    MainConfig = Config.pop(list(Config.keys())[0])
+    FollowConfig = Config
+
     if args.fitness:
         spec=importlib.util.spec_from_file_location('fitness', args.fitness)
         fitness = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(fitness)
-        Cost = dict(inspect.getmembers(fitness))[Config['costfunc']]
+        Cost = dict(inspect.getmembers(fitness))[MainConfig['costfunc']]
     else:
         from moldrug import fitness
-        Cost = dict(inspect.getmembers(fitness))[Config['costfunc']]
+        Cost = dict(inspect.getmembers(fitness))[MainConfig['costfunc']]
     
-    if Config['type'].lower() == 'ga':
+    if MainConfig['type'].lower() == 'ga':
         TypeOfRun = utils.GA
-    elif Config['type'].lower() == 'local':
+    elif MainConfig['type'].lower() == 'local':
         TypeOfRun = utils.Local
     else:
-        raise RuntimeError(f"\"{Config['type']}\" it is not a possible type. Select from: GA or Local")
-    InitArgs = Config.copy()
+        raise RuntimeError(f"\"{MainConfig['type']}\" it is not a possible type. Select from: GA or Local")
+    InitArgs = MainConfig.copy()
 
     # Modifying InitArgs
     [InitArgs.pop(key, None) for key in ['type', 'njobs']]
     InitArgs['costfunc'] = Cost
+
+    # Checking for follow jobs and sanity check on the arguments
+    if FollowConfig:
+        # Defining the possible mutable arguments with its default values
+        mutable_args = {
+            'njobs': MainConfig['njobs'],
+            'crem_db_path': InitArgs['crem_db_path'],
+            'maxiter': InitArgs['maxiter'],
+            'popsize': InitArgs['popsize'],
+            'beta': InitArgs['beta'],
+            'pc': InitArgs['pc'],
+            'get_similar': InitArgs['get_similar'],
+            # This one it will update with the default values of crem rather thant the previous one.
+            'mutate_crem_kwargs': InitArgs['mutate_crem_kwargs'],
+            'save_pop_every_gen': InitArgs['save_pop_every_gen'],
+            'deffnm': InitArgs['deffnm'],
+        }
+        # Sanity check
+        for job in FollowConfig:
+            for arg in FollowConfig[job]:
+                if arg not in mutable_args:
+                    raise ValueError(f"The job: {job} has a non-valid argument \"{arg}\". For now only the following are accepted: {list(mutable_args.keys())}")   
     
     # Initialize the class
-    Results = TypeOfRun(**InitArgs)
+    ResultsClass = TypeOfRun(**InitArgs)
     # Call the class
-    Results(Config['njobs'])
+    print(f'The main job is being executed.')
+    ResultsClass(MainConfig['njobs'])
     # Save final data
-    Results.pickle(f"{InitArgs['deffnm']}_result", compress=True)
-    # Saving final sdf file
-    utils.make_sdf(Results.pop, sdf = f"{InitArgs['deffnm']}_pop.sdf")
-    print(Results.to_dataframe())
- 
+    ResultsClass.pickle(f"{InitArgs['deffnm']}_result", compress=True)
+    # Saving final sdf file always
+    utils.make_sdf(ResultsClass.pop, sdf = f"{InitArgs['deffnm']}_pop.sdf")
+    print(f'The main job finished!.')
+    # In case that follows jobs were defined
+    if FollowConfig:
+        for job in FollowConfig:
+            print(f"The follow job {job} started.")
+            
+            # Updating arguments
+            mutable_args.update(FollowConfig[job])
+            InitArgs = mutable_args.copy()
+            njobs = InitArgs.pop('njobs')
+
+            # Changing the attributes values
+            for arg in InitArgs:
+                setattr(ResultsClass, arg, InitArgs[arg])
+
+            # Call the class again
+            ResultsClass(njobs)
+            # Save final data
+            ResultsClass.pickle(f"{InitArgs['deffnm']}_result", compress=True)
+            # Saving final sdf file always
+            utils.make_sdf(ResultsClass.pop, sdf = f"{InitArgs['deffnm']}_pop.sdf")
+            print(f'The job {job} finished!.')
