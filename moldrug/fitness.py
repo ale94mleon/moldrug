9f01ae3fdb5420d310f25a7ad9dc678d39d34feb
ale94mleon <ale94mleon@gmail.com>
2022-08-25 16:34:21 +0200

    fix bug

--- moldrug/fitness.py
+++ moldrug/fitness.py
@@ -402,9 +402,9 @@ def vinadock(
                 if vina_score < vina_score_pdbqt[0]:
                     if constraint_type == 'local_only':
                         if os.path.isfile(os.path.join(wd, f'{Individual.idx}_conf_{conf.GetId()}_out.pdbqt')):
-                            pdbqt = ''.join(utils.VINA_OUT(os.path.join(wd, f'{Individual.idx}_conf_{conf.GetId()}_out.pdbqt')).BestEnergy())
+                            pdbqt = ''.join(utils.VINA_OUT(os.path.join(wd, f'{Individual.idx}_conf_{conf.GetId()}_out.pdbqt')).BestEnergy().chunk)
                         else:
-                            pdbqt = Individual.pdbqt
+                            pdbqt = "NonExistedFileToRead"
                         vina_score_pdbqt = (vina_score, pdbqt)
                     else:
                         vina_score_pdbqt = (vina_score, preparator.write_pdbqt_string())
