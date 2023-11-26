64ba3b37b6a7eb0f778c9cf1fe46ccfe1ab16893
Alejandro Martinez Leon <ale94mleon@gmail.com>
2023-11-26 10:36:54 +0100

    remove moldrug dependency during vina download

--- tests/get_vina.py
+++ tests/get_vina.py
@@ -1,18 +1,21 @@
+import subprocess
 import sysconfig
 
-from moldrug import utils
+
+def run(command):
+    subprocess.run(command, shell=True, executable='/bin/bash', stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
 
 def download():
     operating_system = sysconfig.get_platform().lower()
 
     if operating_system.startswith('macos'):
-        utils.run("""
+        run("""
             curl -L https://github.com/ccsb-scripps/AutoDock-Vina/releases/download/v1.2.5/vina_1.2.5_mac_x86_64 > vina
             chmod a+x vina
         """)
     elif operating_system.startswith('linux'):
-        utils.run("""
+        run("""
         wget https://github.com/ccsb-scripps/AutoDock-Vina/releases/download/v1.2.5/vina_1.2.5_linux_x86_64
         mv vina_1.2.5_linux_x86_64 vina
         chmod a+x vina
